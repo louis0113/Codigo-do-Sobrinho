75e2d98aa60bf3ae4e3f5b1f9cd06b1547cba5a2
Moises Carlos <moisescarlos664@gmail.com>
2026-01-18 20:34:18 -0300

    Fix: Corrigir erros fatais e de sintaxe em index.php
    Remove função 'encode' incompleta, corrige o uso da superglobal $_SERVER e comenta o código que depende da variável não inicializada $sistema para previnir erros fatais.

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ require 'Controllers/Logistica/Produto.php';
 header('Content-Type: application/json');
 $banana = new Produto(1, "Banana", 2.50);
 
-$request_uri = $_SERVER('REQUEST_URI');
+$request_uri = $_SERVER['REQUEST_URI'];
 
-function encode($obj, $method){
-    json_encode($obj->)
-}
+// A função encode estava incompleta e causando um erro fatal.
+// function encode($obj, $method){
+//     json_encode($obj->)
+// }
 
 switch ($request_uri) {
     case '/' :
@@ -23,17 +24,20 @@ switch ($request_uri) {
 
     case '/atualizarpedido':
         $id = $_GET['id_pedido'];
-        echo json_encode($sistema->processarPagamento($id));
+        // TODO: Inicializar o objeto $sistema antes de usar.
+        // echo json_encode($sistema->processarPagamento($id));
         break;
 
     case 'atualizar_drone':
         $id = $_GET['id_drone'];
         $dados = json_decode(file_get_contents('php://input'), true);
-        echo json_encode($sistema->atualizarDrone($id, $dados));
+        // TODO: Inicializar o objeto $sistema antes de usar.
+        // echo json_encode($sistema->atualizarDrone($id, $dados));
         break;
 
     case 'obter_rotas':
-        echo json_encode($sistema->obterRotas());
+        // TODO: Inicializar o objeto $sistema antes de usar.
+        // echo json_encode($sistema->obterRotas());
         break;
 
     default:
